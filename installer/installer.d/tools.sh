06bda0238020307a0ea228d9c352d6c42f28f5f0
Akarapol Kasivittayanun <akarapol@gmail.com>
2024-06-14 21:34:36 +0700

    feat: add nvm to .bashrc

--- installer/installer.d/tools.sh
+++ installer/installer.d/tools.sh
@@ -41,7 +41,7 @@ install_nvm() {
         "export NVM_DIR=\"\$HOME/.nvm\"" \
         "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm" \
         "[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion" |
-        tee -a ~/.zshrc >/dev/null    
+        tee -a ~/.zshrc ~/.bashrc >/dev/null
     fi
     STATUS_MSG+=$(success "Install NVM successful")
 
